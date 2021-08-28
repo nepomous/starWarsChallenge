46939d39b543c571dafdc13341817fe74885a623
Martins <foliveira.appsimples@gmail.com>
2021-08-28 17:20:19 -0300

    initial commit

---
+++ ios/Pods/Target Support Files/Pods-starWarsChallenge-starWarsChallengeTests/Pods-starWarsChallenge-starWarsChallengeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_starWarsChallenge_starWarsChallengeTests : NSObject
+@end
+@implementation PodsDummy_Pods_starWarsChallenge_starWarsChallengeTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-starWarsChallenge/Pods-starWarsChallenge-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_starWarsChallenge : NSObject
+@end
+@implementation PodsDummy_Pods_starWarsChallenge
+@end
